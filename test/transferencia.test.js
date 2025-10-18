de6cd1769fcc7125301482ed5515dd7b750ef49f
unknown <pamelanmuller@gmail.com>
2025-10-17 23:17:42 -0300

    Adicionei o dontenv

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,11 +1,12 @@
 const request = require('supertest');
 const{ expect } = require('chai')
+require('dotenv').config()
 
 describe('Transferencias', () => {
     describe('POST/Transferencias', () => {
         it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de 10,00 reais', async () => {
             //capturar o token
-            const respostaLogin = await request('http://localhost:3000')
+            const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type','application/json')
                 .send({
@@ -15,7 +16,7 @@ describe('Transferencias', () => {
 
             const token = respostaLogin.body.token
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type','application/json')
                 .set('Authorization','Bearer '+ token)
@@ -32,7 +33,7 @@ describe('Transferencias', () => {
 
         it('Deve retornar 422 quando o valor da transferencia for abaixo de 10,00 reais', async () => {
             //capturar o token
-            const respostaLogin = await request('http://localhost:3000')
+            const respostaLogin = await request(process.env.BASE_URL)
                 .post('/login')
                 .set('Content-Type','application/json')
                 .send({
@@ -42,7 +43,7 @@ describe('Transferencias', () => {
 
             const token = respostaLogin.body.token
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type','application/json')
                 .set('Authorization','Bearer '+ token)
